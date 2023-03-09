2d3deec66b6ba704f6ca5b6ff1e88504833b244c
Unixan <115973884+Unixan@users.noreply.github.com>
2023-03-09 14:48:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Hash
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            for (int i = 4; i >= 0; i--)
+            {
+                AddEmpty(i);
+                AddHash(8 - (2 * i));
+                Console.WriteLine();
+            }
+            
+            for (int i = 0; i < 4; i++)
+            {
+                AddEmpty(i);
+                AddHash(8 - (2 * i));
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        private static void AddEmpty(int index)
+        {
+            for (int i = 0; i < index; i++)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        private static void AddHash(int index)
+        {
+            for (int i = 0; i < index; i++)
+            {
+                Console.Write("#");
+            }
+        }
+
+    }
+}
